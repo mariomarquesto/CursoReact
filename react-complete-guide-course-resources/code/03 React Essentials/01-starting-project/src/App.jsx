dbe368def0d9a96cd2fe7557111297c6ec76dc4c
mariomarquesto <mariomarquesto@gmail.com>
2024-07-28 15:52:11 -0300

    segindo commit

--- react-complete-guide-course-resources/code/03 React Essentials/01-starting-project/src/App.jsx
+++ react-complete-guide-course-resources/code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,14 +1,28 @@
-function App() {
-  return (
-    <div>
-      <header>
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomInt(max){
+  return Math.floor(Math.random() * (max +1));
+}
+
+
+function Header() {
+const description = reactDescriptions[getRandomInt(2)];
+
+  return <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+         {description} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
+}
+
+
+function App() {
+  return (
+    <div>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
